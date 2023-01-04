32d3f77274d107220c918904787e868a4f41341a
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-01-04 15:49:28 +0100

    Added code from Arduino IDE

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
 #include <Arduino.h>
 
+#define SPI_DDR DDRB
+#define SPI_PORT PORTB
+#define MOSI PB3
+#define MISO PB4
+#define SS PB2
+#define SCK PB5
+
+void spi_write(uint8_t data) {
+  SPDR = data;
+}
+
+uint8_t spi_read() {
+  while (!(SPSR & (1 << SPIF))) {
+    delay(5);
+  }
+
+  return SPDR;
+}
+
+void spi_slave_init()
+{
+	// set MISO to output and rest to input
+	SPI_DDR |= (1 << MISO);
+	SPI_DDR &= ~(1 << MOSI) & ~(1 << SCK) & ~(1 << SS);
+	
+	// set MISO to HIGH
+	SPI_PORT |= (1 << MISO);
+	
+	// enable SPI
+	SPCR |= (1 << SPE);
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  spi_slave_init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint8_t spi_data = spi_read();
+  spi_write(~spi_data);
+
+  Serial.print("Received: ");
+  Serial.println(spi_data, HEX);
 }
