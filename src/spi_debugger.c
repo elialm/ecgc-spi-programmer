ed84e8d1de82c81147b193e6588fbe9152c86945
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-02-03 23:41:57 +0100

    Implemented write

--- src/spi_debugger.c
+++ src/spi_debugger.c
@@ -3,6 +3,8 @@
 #include "byte_regex.h"
 #include "spi.h"
 
+#include <string.h>
+
 static int __spi_debugger_send_packet(const uint8_t *write_data, uint8_t *read_data, size_t data_length, const uint8_t *response_pattern)
 {
     // send write_data and read into read_data
@@ -110,10 +112,73 @@ int8_t spi_debugger_set_address(pin_t *cs_pin, pin_t *dbg_pin, uint16_t address)
 
 int8_t spi_debugger_write(pin_t *cs_pin, pin_t *dbg_pin, const uint8_t *data, size_t data_length)
 {
-    return -1;
+    int err;
+    size_t data_index = 0;
+    size_t burst_amount = data_length / 16;
+    size_t individual_amount = data_length % 16;
+    uint8_t read_buffer[16];
+
+    uint8_t burst_response_pattern[18];
+    burst_response_pattern[0] = 0xF1;
+    burst_response_pattern[1] = 0x00;
+    // close_response_pattern[2..16] is set in burst loop
+    burst_response_pattern[17] = 0x00;
+    uint8_t close_response_pattern[4];
+    close_response_pattern[0] = 0x11;
+    // close_response_pattern[1] is set in burst loop
+    close_response_pattern[2] = 0xF1;
+    close_response_pattern[3] = 0x00;
+
+    uint8_t individual_data[2];
+    // individual_data[0] is set in individual loop
+    individual_data[1] = 0x0F;
+    uint8_t individual_response_pattern[4];
+    individual_response_pattern[0] = 0x11;
+    individual_response_pattern[1] = 0x00;
+    // individual_response_pattern[2] is set in individual loop
+    individual_response_pattern[3] = 0x00;
+
+    // write all bursts
+    for (size_t i = 0; i < burst_amount; i++) {
+        // write burst command
+        if ((err = __spi_debugger_send_packet("\x0B\x0F", read_buffer, 2, "\x11\xF1\xB1")) != 0) {
+            return -1;
+        }
+
+        // write burst data
+        memcpy(burst_response_pattern + 2, data + data_index, 15);
+        if ((err = __spi_debugger_send_packet(data + data_index, read_buffer, 16, burst_response_pattern)) != 0) {
+            return -2;
+        }
+
+        // write burst close off
+        burst_response_pattern[1] = data[data_index + 15];
+        if ((err = __spi_debugger_send_packet("\x0F\x0F", read_buffer, 2, burst_response_pattern)) != 0) {
+            return -3;
+        }
+
+        data_index += 16;
+    }
+
+    // write all individuals
+    for (size_t i = 0; i < individual_amount; i++, data_index++) {
+        // write command
+        if ((err = __spi_debugger_send_packet("\x09\x0F", read_buffer, 2, "\x11\xF1\x91")) != 0) {
+            return -4;
+        }
+
+        // write data
+        individual_data[0] = data[data_index];
+        individual_response_pattern[2] = data[data_index];
+        if ((err = __spi_debugger_send_packet(individual_data, read_buffer, 2, individual_response_pattern)) != 0) {
+            return -5;
+        }
+    }
+
+    return 0;
 }
 
-int8_t spi_debugger_write(pin_t *cs_pin, pin_t *dbg_pin, uint8_t *data, const size_t data_length)
+int8_t spi_debugger_read(pin_t *cs_pin, pin_t *dbg_pin, uint8_t *data, const size_t data_length)
 {
     return -1;
 }
